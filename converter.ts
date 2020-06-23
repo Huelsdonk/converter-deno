3c6d7512ebe7ef1259fab02958e2866bb59d4d48
Huelsgoat <huntsperger@gmail.com>
2020-06-23 13:10:02 -0700

    initial working

---
+++ converter.ts
@@ -0,0 +1,55 @@
+import Ask from 'https://deno.land/x/ask/mod.ts';
+
+const ingredients: {[name: string]: number} = {
+    'flour': 120,
+    '00 flour': 106,
+    'whole wheat flour': 113,
+    'sugar': 198,
+    'brown sugar': 213,
+    'butter': 227,
+    'butter tbsp': 14.125,
+    'brown butter': 182,
+    'powdered sugar': 230,
+    'water': 227,
+    'milk': 227,
+    'cream': 227,
+    'graham crackers': 142,
+    'corn syrup': 312,
+    'eggs': 57,
+    'cocoa': 85,
+    'oats': 99,
+    'shortening': 184,
+    'chocolate chips': 170,
+    'coconut': 85,
+    'coconut oil': 156
+};
+
+
+const ask = new Ask();
+
+const whichOne = await ask.input({
+    
+      name: 'ingredient',
+      type: 'string',
+      message: 'Which ingredient would you like to convert?'
+    
+});
+
+function convert(amount: number, ing: number) {
+    return `That is ${amount * ing} grams`;
+}
+
+
+
+if (Object.keys(ingredients).includes(whichOne.ingredient)) {
+    const howMuch = await ask.input({
+        name: 'amount',
+        type: 'number',
+        message: 'Enter an amount (usually in cups):'
+    });
+    console.log(convert(howMuch.amount, ingredients[whichOne.ingredient]))
+} else {
+    console.log("I can't convert that.")
+}
+
+  
